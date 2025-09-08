2d942470fae7729d1607611668093c7eeee731ba
Alex Ignacio <alexander.n.ignacio@gmail.com>
2025-09-08 18:11:23 -0400

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  return (
+    <div name="body" style={{
+       display: "flex",
+       justifyContent: "center",
+       flexDirection: "column",
+       alignItems: "center",
+       height: "100vh",
+       width: "100vw",
+       border: "1px solid black",
+      }}>
+
+      <div>
+        <h1>Hello World</h1>
+      </div>
+
+      <div>
+        <p>Im Alex Ignacio</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App
