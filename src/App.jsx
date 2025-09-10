21a5dfedddf65626816b45baa7020fd4b0b860d8
Alex Ignacio <alexander.n.ignacio@gmail.com>
2025-09-10 14:19:01 -0400

    Created and styled website

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import './App.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 function App() {
   return (
-    <div name="body" style={{
-       display: "flex",
-       justifyContent: "center",
-       flexDirection: "column",
-       alignItems: "center",
-       height: "100vh",
-       width: "100vw",
-       border: "1px solid black",
-      }}>
-
-      <div>
-        <h1>Hello World</h1>
+    <div className="container">
+      <div className='title' >
+          <h1>Hi, I'm Alex Ignacio</h1>
       </div>
 
-      <div>
-        <p>Im Alex Ignacio</p>
+      <div className="message">
+        <h3>Welcome to my website</h3>
+      </div>
+
+      <div className="icon-container">
+        <div className="AccountCircleIcon">
+          <AccountCircleIcon sx={{ fontSize: 140 }}/>
+        </div>
       </div>
 
     </div>
+
   );
 }
 
