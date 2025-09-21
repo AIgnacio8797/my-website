4e21b68bacfb28c2404dc2d4fc46042d3d959e25
Alex Ignacio <alexander.n.ignacio@gmail.com>
2025-09-21 13:44:25 -0400

    CSS changes

--- src/QuickLinks.jsx
+++ src/QuickLinks.jsx
@@ -3,13 +3,20 @@ import image from './Images/github-logo.png'
 
 function QuickLinks(){
     return (
-        <div className="quick-links" style={{ display: "flex", justifyContent: "flex-end" }}>
-            <div className="git-hub" style={{ display: "flex", alignItems: "center" }}>
-                <img src={image} alt="GitHub Logo"
-                style={{ filter: 'invert(1)', height: '24px', width: '24px', marginRight: '18px'}} />
-                <a href="https://github.com/AIgnacio8797" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <div>
+            <div className="quick-links" style={{ display: "flex", justifyContent: "flex-end" }}>
+                <div className="git-hub" style={{ display: "flex", alignItems: "center" }}>
+                    <img src={image} alt="GitHub Logo"
+                    style={{ filter: 'invert(1)', height: '24px', width: '24px', marginRight: '18px'}} />
+                    <a href="https://github.com/AIgnacio8797" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </div>
+            </div>
+
+            <div className="email">
+                
             </div>
         </div>
+
     )
 }
 
